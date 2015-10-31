6701bfe07d68763f6f5fac3f79a830b361e59692
Jeanlo <jeanlotejeda0101@hotmail.com>
2015-10-30 22:36:02 -0400

    Main menu
    Agregado el menu principal giratorio (solo sirve hacia la derecha)

---
+++ Assets/Scripts/CambiarActuacion.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class CambiarActuacion : MonoBehaviour {
+    public static int Movidos { get; set; }
+    public static bool LetsMove { get; set; }
+    public Vector3 PosicionInicio { get; set; }
+    public DateTime TiempoUltimaActualizacion { get; set; }
+    public static string Actual { get; set; }
+    void Start ()
+    {
+        PosicionInicio = transform.localPosition;
+        TiempoUltimaActualizacion = DateTime.Now;
+    }
+
+	void Update ()
+    {
+        MoverCamara();
+	}
+
+    public void MoverCamara()
+    {
+        if (transform.localPosition == Vector3.zero)
+            Actual = this.name; 
+        int ultimo = 3;
+        float tecla = Input.GetAxis("Horizontal");
+        if (tecla < 0)
+            tecla = -1;
+        if (tecla > 0)
+            tecla = 1;
+
+        if (tecla != 0 && DateTime.Now.Subtract(TiempoUltimaActualizacion) > TimeSpan.FromSeconds(0.3))
+        {
+            if (this.gameObject.name == "main-menu-actuacion" + ultimo.ToString() && transform.localPosition == Vector3.zero)            
+                LetsMove = true;
+            
+            if (LetsMove && transform.localPosition != PosicionInicio)
+            {
+                transform.localPosition = PosicionInicio;
+                TiempoUltimaActualizacion = DateTime.Now;
+                Movidos++;
+                return;
+            }
+
+            if (LetsMove && Movidos == ultimo)
+            {
+                LetsMove = false;
+                Movidos = 0;
+            }
+
+            transform.Translate(tecla * 18.5f, 0, 0);
+            TiempoUltimaActualizacion = DateTime.Now;
+        }
+    }
+}
---
+++ Assets/Scripts/LevantarTelon.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevantarTelon : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void CargarActuacion()
+    {
+        switch (CambiarActuacion.Actual)
+        {
+            case "main-menu-actuacion1":
+                Application.LoadLevel(1);
+                break;
+
+            case "main-menu-actuacion2":
+                Application.LoadLevel(2);
+                break;
+
+            case "main-menu-actuacion3":
+                Application.LoadLevel(3);
+                break;
+        }
+    }
+}
