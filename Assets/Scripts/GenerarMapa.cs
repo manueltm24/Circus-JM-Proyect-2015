d38ea39ff555080b596d0cd8d7e605b1821ea110
manueltm24 <manutm24@gmail.com>
2015-11-16 15:42:08 -0400

    Revert "Jarrones agregados"
    This reverts commit 3042bc50c12d71adf050a735f3093e2bc8de38d5.

--- Assets/Scripts/GenerarMapa.cs
+++ Assets/Scripts/GenerarMapa.cs
@@ -37,10 +37,6 @@ public class GenerarMapa : MonoBehaviour {
                         Instanciador(i, PFs[1]);
                         break;
 
-                    case '.':
-                        Instanciador(i, PFs[2]);
-                        break;
-
                     case 'P':
                         Instanciador(i, PFs.First(P => P.name.Contains("Personaje")));
                         break;
